5b335f70f2e357c376846967e65f98ea8485917e
Jonathan Wills <runningwild@gmail.com>
2011-11-22 12:27:58 -0800

    Trying to make things goinstallable

---
+++ polish/all_specs_test.go
@@ -0,0 +1,13 @@
+package fft_test
+
+import (
+  "gospec"
+  "testing"
+)
+
+func TestAllSpecs(t *testing.T) {
+  r := gospec.NewRunner()
+  r.AddSpec(Float64ContextSpec)
+  r.AddSpec(IntContextSpec)
+  gospec.MainGoTest(r, t)
+}
